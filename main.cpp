29ac13ce1996e9a6abee2cb13cbecba612f42d12
Daniel Douglas <daniel.douglas@drexel.edu>
2015-01-29 01:40:50 -0500

    Okay, so I lied... Fixed some stupid stuff

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
 #include <fstream>
-#include <string>
-#include <vector>
 
+// Define some maps here!
 float logistic(float x, float lambda) {
 	return lambda*x*(1-x);
 }
@@ -11,12 +9,12 @@ float fold(float y, float a) {
 	return a - y*y;
 }
 
+// This just iterates a given map i times
 float iterate(float (*map)(float, float), float x, float a, int i) {
 	float newx = map(x, a);
 	if ( i ) return iterate(map, newx, a, i-1);
 	else return x;
 }
-
 
 int main() {
 	std::ofstream outFile;
@@ -25,8 +23,8 @@ int main() {
 	for ( float lambda = 1; lambda < 4; lambda += 0.01 ) {
 		outFile <<  lambda << '\t';
 		for ( float x = 0.001; x < 1; x += 0.01 ) {
+			// Set up right now for 3 iterations of the logistic map
 			outFile << iterate(logistic, x, lambda, 3) << '\t';
-//			outFile << logistic(logistic(logistic(x, lambda), lambda), lambda) << '\t';
 		}
 		outFile << '\n';
 	}
--- makefile
+++ makefile
@@ -2,13 +2,16 @@ all: mapmaker
 
 mapmaker: main.cpp
 	g++ main.cpp -o mapmaker
+	echo "Compiled! Now do 'make run'"
 
 run:
 	./mapmaker
+	echo "Done! Now do 'make plot'"
 
 plot: plot.gp map.dat
 	gnuplot plot.gp
-	eog *.jpg &
+	display map.jpg &
+	echo "Plotted! File saved as map.jpg!"
 
 clean:
 	rm -rf *~ *.o *.dat *.png *.aux *.log
